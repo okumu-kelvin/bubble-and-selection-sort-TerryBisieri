5a25d5b59ce4f233cdda16da80d2860ea5e9c1aa
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-06-20 09:46:24 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -14,5 +14,4 @@ def bubble_sort(unsorted_list):
 
 My_list=[3, 2, 1, 9, 5, 7, 8, 6]
 print("Unsorted List: ", My_list)
-sorted_list=bubble_sort(My_list)
-print("Sorted List: ",sorted_list)
+print("Sorted List: ",bubble_sort(My_list))
