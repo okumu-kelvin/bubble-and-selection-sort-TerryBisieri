190f852b93e0cd4cf7094a82e8f7746306a3c6ab
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-06-20 09:24:33 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,17 +1,17 @@
-def bubble_sort(arr):
+def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-        n = len(arr)
+        n = len(unsorted_list)
         for i in range(n):
             # Track if any swap was made
             swapped = False
             for j in range(0, n - i - 1):
-                if arr[j] > arr[j + 1]:
+                if unsorted_list[j] > unsorted_list[j + 1]:
                     # Swap elements
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    unsorted_list[j], [j + 1] = unsorted_list[j + 1], unsorted_list[j]
                     swapped = True
             if not swapped:
                 break
-        return arr
+        return unsorted_list
 
 My_list=[3, 2, 1, 9, 5, 7, 8, 6]
 print("Unsorted List: ", My_list)
