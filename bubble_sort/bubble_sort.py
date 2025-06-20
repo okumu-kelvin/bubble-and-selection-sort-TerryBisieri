be1689ee6b67ef44f5fdf0c9a22149d06d0c98fe
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-06-20 10:01:32 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,15 +1,16 @@
 def bubble_sort(unsorted_list):
-        n = len(unsorted_list)
-        for i in range(n):
-            swapped = False
-            for j in range(0, n - i - 1):
-                if unsorted_list[j] > unsorted_list[j + 1]:
-                    unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1],unsorted_list[j]
-                    swapped = True
-            if not swapped:
-                break
-        return unsorted_list
+    n = len(unsorted_list)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                swapped = True
+        if not swapped:
+            break
+    return unsorted_list
 
-My_list=[3, 2, 1, 9, 5, 7, 8, 6]
-print("Unsorted List: ", My_list)
-print("Sorted List: ",bubble_sort(My_list.copy()))
+data = [6,5, 1, 2, 7]
+
+print("Original list :", data)
+print("Bubble Sort:", bubble_sort(data.copy()))
